1283e29a434ffe8a443807d3d0a16d14ef233717
nabekabebe <nabekabebe6@gmail.com>
2021-12-11 14:26:45 +0300

    merge Intervals.

---
+++ week1/mergeIntervals.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        intervals.sort()
+        
+        lst = []
+        count = 0
+        if len(intervals) == 1:
+            return intervals
+        
+        a = intervals[0]
+        for i in range(1, len(intervals)):
+           
+            if a[1] >= intervals[i][0]:
+                a = [a[0], intervals[i][1] if (intervals[i][1] > a[1]) else a[1] ]  
+                if i == (len(intervals)-1):
+                    lst.append(a)
+            else:
+                if i == (len(intervals)-1):
+                    lst.append(intervals[i])
+                lst.append(a)
+                a = intervals[i]
+                
+        return lst
